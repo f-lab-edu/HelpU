f0c8c272740909698fa07eb02d0365cb73b75720
geol2 <big9401@gmail.com>
2023-02-26 22:10:50 +0900

    [세팅] 스프링 Initializr 를 이용한 초기화

---
+++ src/main/java/com/helpu/helpu/HelpuApplication.java
@@ -0,0 +1,13 @@
+package com.helpu.helpu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helpu/helpu/HelpuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpu.helpu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
