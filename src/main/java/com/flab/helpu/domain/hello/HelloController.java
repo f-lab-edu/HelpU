ef41b52af595fdeca4873602345d0dc5d3054963
geol2 <big9401@gmail.com>
2023-03-05 22:42:37 +0900

    [세팅] 어노테이션 설명 추가 - @Controller, @RequestMapping, @SpringBootTest, @Test, @SpringBootApplication 추가

--- src/main/java/com/flab/helpu/domain/hello/HelloController.java
+++ src/main/java/com/flab/helpu/domain/hello/HelloController.java
@@ -4,13 +4,20 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-// 스프링에서 컨트롤러임을 나타내고 빈에 등록
+/** <strong> Annotation Note : </strong>
+ * @Controller : @Component 의 특수한 역할을 하여 클래스 경로 스캔을 통해 클래스를 자동으로 감지할 수 있다
+ * @RequestMapping 어노테이션을 감자하기도 한다
+ */
 @Controller
 public class HelloController {
 
-  // 요청을 처리하는 어노테이션
-  @RequestMapping("/")
+  /** <strong> Annotation Note : </strong>
+   *  @RequestMapping : URL을 컨트롤러의 메소드와 매핑할 때 사용하는 어노테이션으로
+   *  클래스나 메소드에서 사용할 수 있다
+   */
+  @RequestMapping(value = "/", method={RequestMethod.GET})
   public void printHelloWorld(HttpServletResponse response) throws IOException {
     response.getWriter().print("Hello World");
   }
--- src/test/java/com/flab/helpu/HelpuApplicationTests.java
+++ src/test/java/com/flab/helpu/HelpuApplicationTests.java
@@ -3,11 +3,17 @@ package com.flab.helpu;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/** <strong>Annotation Note : </strong>
+ * @SpringBootTest : 스프링부트를 기반으로 한 테스트를 실행하기 위해 클래스에 지정할 수 있는 주석
+ */
 @SpringBootTest
 class HelpuApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	/** <strong>Annotation Note : </strong>
+	 * @Test : 테스트 메소드임을 명시해주는 어노테이션
+	 * 해당 어노테이션은 private, static 이 불가능하고 값을 반환하면 안된다
+	 *
+	 */
+  @Test
+  void contextLoads() {
+  }
 }
